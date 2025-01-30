d516199736a03e568f1d3f1f513ae9835648c34f
cybersnacker <dimpy_1991@yahoo.co.in>
2025-01-29 19:02:07 -0500

    Shopping Assistant

---
+++ crewai/shopping_assistant/src/shopping_assistant/main.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+import sys
+import warnings
+
+from datetime import datetime
+
+from shopping_assistant.crew import ShoppingAssistant
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'requirements': 'Macbook with at least M3 CPU, 256GB RAM, at least 16GB memory and 2 display ports',
+    }
+    
+    try:
+        ShoppingAssistant().crew().kickoff(inputs=inputs)
+    except Exception as e:
+        raise Exception(f"An error occurred while running the crew: {e}")
+
+
+# def train():
+#     """
+#     Train the crew for a given number of iterations.
+#     """
+#     inputs = {
+#         "topic": "AI LLMs"
+#     }
+#     try:
+#         ShoppingAssistant().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+#     except Exception as e:
+#         raise Exception(f"An error occurred while training the crew: {e}")
+
+# def replay():
+#     """
+#     Replay the crew execution from a specific task.
+#     """
+#     try:
+#         ShoppingAssistant().crew().replay(task_id=sys.argv[1])
+
+#     except Exception as e:
+#         raise Exception(f"An error occurred while replaying the crew: {e}")
+
+# def test():
+#     """
+#     Test the crew execution and returns the results.
+#     """
+#     inputs = {
+#         "topic": "AI LLMs"
+#     }
+#     try:
+#         ShoppingAssistant().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
+
+#     except Exception as e:
+#         raise Exception(f"An error occurred while testing the crew: {e}")
